a9467f432000da0462a27b1075fe834c32bac0ba
zeus <zeusofolympus1000@gmail.com>
2022-07-19 23:15:36 -0400

    were going away from a s purechehema model for now, and using an object schema model

--- Source/Book.js
+++ Source/Book.js
@@ -1,52 +1,35 @@
 import * as assert from "node:assert"
 import * as util from "node:util"
 import * as fs from "node:fs"
-
+import {Scroll} from "./Scroll.js"
 
 //Its just a book...
 export class Book{
     constructor(file, string, tools){
-        this.tools=tools;
-        this._Scroll=new Scroll(string)
-        if(file && string){
-            tools=string
-            string=file
-        }
-
-        this.book={
-            'index':{
-
-            },
-            'pages':{
-                
-            }
-        }
-        this.index;//for the future!
-        if(file&&string){
-            this.bookify(string, tools, this);
-        }else{
-            this.import(file, this)
-        }
+		this.index;
+		this.pages;
+		if(!string){
+			this.index=tools['index'];
+			this.pages=tools['pages'];
+		}
 
     }
 
     //PUBLIC GETTERS
-    getPageCount(_Book){
-        if(!_Book){_Book=this;}
-		return parseInt(_Book.book['pageCount']);
+    getPageCount(){
+		return parseInt(this.book['pageCount']);
 	}
 	getLineCount(page){
         return parseInt(page['lineCount']);
     }
 
     //PUBLIC UTILS
-    bookify(string, tools, _Book){
-        if(!_Book){_Book=this;}
+    bookify(string, tools){
 		if(!(string&&tools)){
 			tools = this.tools;
 		}
-		if(!_Book.book){
-			_Book.book=this._getEmptyBook();
+		if(!this.book){
+			this.book=this._getEmptyBook();
 		}
 		var page=this._getEmptyPage();
 
@@ -58,7 +41,7 @@ export class Book{
 					line+=string[i];	//adds the anchor to the string
 					this._enscribeLineToPage(line, page);
                     this._setCharOffsetToPage(i, page);
-					this._addPageToBook(page, _Book);
+					this._addPageToBook(page);
 					page=this._getEmptyPage();
 					line="";
 				//LEAVE THIS -1 after tools['lineCount'] because we are looking for anything BEFORE THE LAST enscribe TO THE QUEUE!
@@ -72,95 +55,87 @@ export class Book{
         	}
 			//THIS IS ALWAYS HIDDEN
             this._enscribeLineToPage(line, page);
-			this._addPageToBook(page, _Book);
+			this._addPageToBook(page);
 		}
 	}
 
-    import(file, _Book, virtualize=false){
-        if(!_Book){_Book=this;}
-        _Book.book = JSON.parse(fs.readFileSync(file))
+    import(file){
+        this.book = JSON.parse(fs.readFileSync(file))
     }
-    exportToBookFile(fileName, _Book){
-        if(!_Book){_Book=this;}
-        fs.writeFileSync(fileName+'.book', JSON.stringify(_Book.book))
+    export(fileName){
+        fs.writeFileSync(fileName+'.book', JSON.stringify(this.book))
     }
     
     virtualize(string){
         
     }
+	crssRef(_Book){
 
-    enscribeToBook(string, tools, _Book){
-        if(!_Book){_Book=this;}
-        if(!string && !_Book.book){
-            throw Error("you need to provide a string and a book");
+	}
+
+    write2Bk(string, tools){
+        if(!string){
+            throw Error("you need to provide a string");
         }
         if(!tools){
             tools=this.tools;
         }
-		this.bookify(string, tools, _Book);
+		this.bookify(string, tools);
 	}
 
-    printBook(_Book){
-        if(!_Book){_Book=this;}
-		console.log(util.inspect(_Book.book, {showHidden: true, depth: null, colors: true}));
+    printBk(){
+		console.log(util.inspect(this.book, {showHidden: true, depth: null, colors: true}));
 	}
 
     //PRIVATE GETTERS
-    _getLineNFromPageM(pageM, lineN, _Book){
-        if(!_Book){_Book=this;}
-        return this.getPageN(pageM, _Book)['lines'][lineN.toString];
+    getLnNPgM(pageM, lineN){
+        return this.getPageN(pageM, this)['lines'][lineN.toString];
     }
 
-    _getPageN(pageN, _Book){
-        if(!_Book){_Book=this;}
-        return _Book.book['pages'][pageN.toString()];
+    getPgN(pageN){
+        return this.book['pages'][pageN.toString()];
     }
 
-    _getEmptyLine(){
+    _getEmtyLn(){
         return {'charCount':'0', 'line':""}
     }
-    _getEmptyPage(){
+    _getEmtyPg(){
 		return {'lineCount':'0','lines':{}, 'charOffset':'0'};
 	}
-	_getEmptyBook(){
+	_getEmtyBk(){
 		return {'pageCount':'0','pages':{}};
 	}
     
-    _removePgsNM(pageN, pageM, _Book){
-        if(!_Book){_Book=this;}
+    rmvPgsNM(pageN, pageM){
 		assert.equal(pageM>=pageN, true);
 		for (var i=pageN; i<=pageM; i++){
-			this._removePgN(i, _Book);
+			this._rmvPgN(i);
 		}
 	}
 
-	_removePgN(n, _Book){
-        if(!_Book){_Book=this;}
-		delete _Book.book['pages'][n.toString()];
-		var tmp = _Book.book['pages'][(n+1).toString()];
-		delete _Book.book['pages'][(n+1).toString()];
-		_Book.book['pages'][n.toString()]=tmp;
-		_Book.book['pageCount']=(parseInt(_Book.book['pageCount'])-1).toString();
+	rmvPgN(n){
+		delete this.book['pages'][n.toString()];
+		var tmp = this.book['pages'][(n+1).toString()];
+		delete this.book['pages'][(n+1).toString()];
+		this.book['pages'][n.toString()]=tmp;
+		this.book['pageCount']=(parseInt(this.book['pageCount'])-1).toString();
 	}
 
     //this should be tested when its actually used, leave it here for now.
-    _popNPgs(nPages, _Book){
-        if(!_Book){_Book=this;}
+    popNPgs(nPages){
 		for(var i = 0; i<nPages; i++){
-			this._popPg(_Book);
+			this._popPg();
 		}
 	}
 	
-    _popPg(_Book){
-        if(!_Book){_Book=this;}
-        delete _Book.book['pages'][_Book.book['pageCount']]; 
-        _Book.book['pageCount']=(parseInt(_Book.book['pageCount'])-1).toString();
+    popPg(){
+        delete this.book['pages'][this.book['pageCount']]; 
+    	this.book['pageCount']=(parseInt(this.book['pageCount'])-1).toString();
     }
 
-    _pushPg(page, _Book){
-        if(!_Book){_Book=this;}
-        _Book.book['pages'][(parseInt(_Book.book['pageCount'])+1).toString()]=page;
-        _Book.book['pageCount']=(parseInt(_Book.book['pageCount'])+1).toString();
+    _pushPg(page){
+        this.book['pages'][(parseInt(this.book['pageCount'])+1).toString()]=page;
+        this.book['pageCount']=(parseInt(this.book['pageCount'])+1).toString();
     }
     _enscribeLineToPage(line, page){
         page['lines'][(parseInt(page['lineCount'])+1).toString()]=line;
