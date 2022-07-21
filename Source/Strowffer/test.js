00804716f45fa1e4a2442ca87b82a876654998b3
zeus <zeusofolympus1000@gmail.com>
2022-07-21 09:36:56 -0400

    using assertions throw and doesnotthrow

--- Source/Strowffer/test.js
+++ Source/Strowffer/test.js
@@ -13,98 +13,132 @@ class StrowfferTest{
 	}
 
 	rowInterface(){
+		
 		console.log('rowInterface() with full array, full type array, full index string, SHOULD PASS')
-		var stwfr = new Strowffer(
+		assert.doesNotThrow(
+			new Strowffer(
 			[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 
 			['int', 'str', 'utf-8', 'int', 'str', 'utf-8', 'int', 'str', 'utf-8', 'int'], 
 			'arbitrary row data'
+			)
 		)
 
-		
-
 		console.log('rowInterface() with full array, null type, null index, SHOULD PASS')
-		stwfr = new Strowffer(
+		assert.doesNotThrow(
+			new Strowffer(
 			[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+			)
 		)
 
 		console.log('rowInterface() with empty array, empty type array, empty index string, SHOULD FAIL')
-
-		try{
-			stwfr = new Strowffer(
+		assert.throws(
+			new Strowffer(
 				[],
 				[],
 				''
 			)
-		}catch(err){
-			console.log("FAIL")
-
-		}
-
+		)
+		
 		console.log('rowInterface() with full array, index replaces type, SHOULD FAIL')
-		try{
-			stwfr = new Strowffer(
+		assert.throws(
+			new Strowffer(
 			[1, 2, 3, 4, 5, 6, 7, 8, 9, 10],  
 			'arbitrary row data'
 			)
-		}catch(err){
-			console.log("FAIL")
-		}
+		)
+
 
 		console.log('rowInterface() with full array, empty type array, empty index string, SHOULD FAIL')
-		try{
-			stwfr = new Strowffer(
+		assert.throws(
+			new Strowffer(
 				[1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
 				[],
 				''
 			)
-		}catch(err){
-			console.log("FAIL")
-		}
+		)
 
 		console.log('rowInterface() with empty array, full type array, empty index string, SHOULD FAIL')
-		try{
+		assert.throws(
 			stwfr = new Strowffer(
 				[],
 				['int', 'str', 'utf-8', 'int', 'str', 'utf-8', 'int', 'str', 'utf-8', 'int'], 
 				''
 			)
-		}catch(err){
-			console.log("FAIL")
-		}
+		)
 
 	}
 
 	stringInterface(){
 		console.log('stringInterface() with full string, full type, full index, SHOULD PASS')
-		var stwfr = new Strowffer("hello world!", 'utf-8', 'greeting')
-		console.log('stringInterface() with full string, full type, null index, SHOULD PASS')
-		stwfr = new Strowffer("hello world!", 'utf-8')
-		console.log('stringInterface() with full string, full type, null index, SHOULD PASS')
-		stwfr = new Strowffer("hello world!")
+		assert.doesNotThrow(
+			new Strowffer("hello world!", 'utf-8', 'greeting')
+		)
 
-		try{
-			console.log('stringInterface() with empty string, empty type, empty index, SHOULD FAIL')
-			stwfr = new Strowffer("", '', '')
-		}catch(err){
-			console.log("FAIL")
-		}
+		console.log('stringInterface() with full string, full type, null index, SHOULD PASS')
+		assert.doesNotThrow(
+			new Strowffer("hello world!", 'utf-8')
+		)
 
-		try{
-			console.log('stringInterface() with empty string, full type, full index, SHOULD FAIL')
-			stwfr = new Strowffer("", "utf-8", "full index")
-		}catch(err){
-			console.log("FAIL")
-		}
+		console.log('stringInterface() with full string, full type, null index, SHOULD PASS')
+		assert.doesNotThrow(
+			new Strowffer("hello world!")
+		)
+
+		console.log('stringInterface() with empty string, empty type, empty index, SHOULD FAIL')
+		assert.throws(
+			new Strowffer("", '', '')
+		)
+
+		console.log('stringInterface() with empty string, full type, full index, SHOULD FAIL')
+		assert.throws(
+			new Strowffer("", "utf-8", "full index")
+		)
+
 	}
 
 	bufferInterface(){
+		console.log('bufferInterface() with full string, full type, full index, SHOULD PASS')
+		assert.doesNotThrow(
+			new Strowffer(Buffer.from("hello world!"), 'utf-8', 'greeting')
+		)
+
+		console.log('bufferInterface() with full string, full type, no index, SHOULD PASS')
+		assert.doesNotThrow(
+			new Strowffer(Buffer.from("hello world!"), 'utf-8')
+		)
+
+		console.log('bufferInterface() with full string, no type, no index, SHOULD PASS')
+		assert.doesNotThrow(
+			new Strowffer(Buffer.from("hello world!"))
+		)
+
+
+
+		console.log('bufferInterface() with full string, empty type, full index, SHOULD FAIL')
+		assert.throws(
+			new Strowffer(Buffer.from("hello world!"), '', 'greeting')
+		)
+
+		console.log('bufferInterface() with full string, empty type, empty index, SHOULD FAIL')
+		assert.throws(
+			new Strowffer(Buffer.from("hello world!"), '', '')
+		)
+
+		console.log('bufferInterface() with empty buffer, full type, full index, SHOULD FAIL')
+		assert.throws(
+			new Strowffer(Buffer.from(""), 'utf-8', 'greeting')
+		)
+
+		console.log('bufferInterface() with nothing, SHOULD FAIL')
+		assert.throws(
+			new Strowffer()
+		)
 
 	}
 
 
-
-
 	rowData(){
+
 
 	}
 
