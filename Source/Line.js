ce8f3e9be7e37bdc1d957326a60e24c6f3ae2aa6
zeus <zeusofolympus1000@gmail.com>
2022-07-21 03:32:21 -0400

    i think this would be more powerful

--- Source/Line.js
+++ Source/Line.js
@@ -1,21 +1,26 @@
 import { Cell } from "./WordCell.js"
-//input
-//if we iterate and fill inside the class from a constructor string, it requires a single encoding
-//if we are iterating and filling from the outside of the class, we have options for encoding for dataframes
-//output
-//there are different usecases
+//a Node Buffer is just an array of decimals that represent byte values, and can be mapped to their original encoding
+//They use decimal values because they are already represented in memory as bytes, but are more efficiently represented
+//at a higher level as decimal
+
 export class Strow{
-    constructor(strow, indx={}, cntxt, encdng="utf-8"){
+    constructor(strow, type, indx=""){
 		if(cntxt=='str' && strow){
 			//if cntxt is a str its just a string in the strow variable
-		}else if(cntxt=='row' && this.isString(strow)){
-			//if cntxt is 'row', with strow as a string, we cellify the string with the same encoding
-		}else if (cntxt=='row' && this.isWordCell()){
-			//if cntxt is 'row' with a strow as cell schema, we cellify the strow schema
+		}else if(this.isString(strow)){
+			//if its a string, its stored locally as a string for optimization
+		}else if (this.isArray(strow) && indx){
+			//if its an array, each element of the array is in its own wordCell. if there is an index for the row, set it
+		}else if (this.isBuffer(strow)&&this.isEncoding(type)){
+			//if its a buffer, its stored locally with its encoding
 
+		}else{
+			throw Error("strow must be a string, array, or buffer type")
 		}
     }
 
-
-
 }
+
+new Strow([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+new Strow("hello world!")
+new Strow(Buffer.from([10, 20, 30, 40, 50, 60, 70, 80, 90, 100]))
--- Source/WordCell.js
+++ Source/WordCell.js
@@ -1,17 +1,7 @@
-//a Node Buffer is just an array of decimals that represent byte values, and can be mapped to their original encoding
-//They use decimal values because they are already represented in memory as bytes, but are more efficiently represented
-//at a higher level as decimal
-
 export class WordCell{
-    constructor(bufr, encdng){
-		//a bufr is an array of bytes
-		//if you knew the encdng of a string of text, why would you ever put things with multiple encodings in the same wordcell?
-		//you wouldn't. So, encdng only represents the entire buffer's encoding here.
-		this.encdng=encdng
-		this.wc=bufr
-	}
-
-	stringify(bufr){
-		console.log(bufr.toString(this.encdng))
+	//type can  be anything, and if we don't support the type, it just looks like whatever comes in	
+	constructor(val, type){
+		this.val=val
+		this.type=type
 	}
 }
