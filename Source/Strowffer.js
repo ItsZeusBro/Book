36165ec5ad12e703ac437648e45ccdb6651b77bd
zeus <zeusofolympus1000@gmail.com>
2022-07-21 04:32:16 -0400

    srf is too profane, srb is better

--- Source/Strowffer.js
+++ Source/Strowffer.js
@@ -5,32 +5,32 @@ import { Cell } from "./Cell.js"
 
 export class Strowffer{
     constructor(strow, type, indx=""){
-		this.s;//strow = string, buffer, or an array
+		this.srb;//strow = string, buffer, or an array
 		this.t;//type
 		this.i;//index
 		this.c;//context
 		if(this.isString(strow)&&this.isEncoding(type)){
 			//if its a string, its stored locally as a string for optimization
 			//String operations can be performed in Strow class as well
-			this.s=strow
+			this.srb=strow
 			this.t=type
 			this.c='string'
 			if(indx){this.i=indx}
 		}else if (this.isArray(strow) && type){
 			//if its an array, each element of the array is in its own cell. if there is an index for the row, set it
 			//rowise operations can be performed in Strow class
-			this.s=[]
+			this.srb=[]
 			if(!type){
 				//type is utf-8
 				for(var i=0; i<strow.length; i++){
-					this.s.push(new Cell(strow[i], 'utf-8'))
+					this.srb.push(new Cell(strow[i], 'utf-8'))
 				}
 			}else{
 				if(this.isArray(type)){
 					//if type is array, check to make sure length is equal to strow length
 					if((type.length==strow.length)){
 						for(var i=0; i<strow.length; i++){
-							this.s.push(new Cell(strow[i], type[i]))
+							this.srb.push(new Cell(strow[i], type[i]))
 						}
 					}else{
 						throw Error("If type is an array, it must be of length strow.length")
@@ -38,7 +38,7 @@ export class Strowffer{
 				}else{
 					//if type is not array, just set the type for each strow to be of type type
 					for(var i=0; i<strow.length; i++){
-						this.s.push(new Cell(strow[i], type))
+						this.srb.push(new Cell(strow[i], type))
 					}
 				}
 			}
@@ -50,7 +50,7 @@ export class Strowffer{
 			//if its a buffer, its stored locally with its encoding. 
 			//It's decoded with strngfy()
 			//its accessed with raw()
-			this.s=strow
+			this.srb=strow
 			this.t=type
 			this.c='buff'
 			if(indx){this.i=indx}
@@ -67,5 +67,5 @@ export class Strowffer{
 }
 
 console.log(new Strowffer([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], ['int', 'str', 'utf-8', 'int', 'str', 'utf-8', 'int', 'str', 'utf-8', 'int'], 'arbitrary row data'))
-console.log(new Strowffer("hello world!", 'utf-8'))
-console.log(new Strowffer(Buffer.from([10, 20, 30, 40, 50, 60, 70, 80, 90, 100]), 'utf-8'))
+console.log(new Strowffer("hello world!", 'utf-8', 'greeting'))
+console.log(new Strowffer(Buffer.from([10, 20, 30, 40, 50, 60, 70, 80, 90, 100]), 'utf-8', 'arbitrary buffer'))
