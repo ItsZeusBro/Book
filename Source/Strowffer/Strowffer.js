e827e6df6636cdbb035536ba51d6296a5ddadcec
Zeus <zeusofolympus1000@gmail.com>
2022-07-22 02:52:17 -0400

    there is no better time for premature optimization than right now!

--- Source/Strowffer/Strowffer.js
+++ Source/Strowffer/Strowffer.js
@@ -2,129 +2,38 @@ import { Cell } from "../Cell.js"
 //Because there is a mathematical relationship between strings, arrays (rows), and buffers
 //we use a Strowffer, even though we do not know how to exploit those mathematical relationships
 //yet. The idea is that if we use them enough, we will find how to exploit that more. 
-export class Strowffer{
+export class Strowfer{
+	//if there are no separators defined for a string (if strwfr is a string)
+	//the string is held in a single cell
+	//if the strwfr is an array or buffer, it is (decoded if a buffer and) separated into cells 
     constructor(strwfr, type='utf-8', indx=''){
 		this.strwfr;//strwfr = string, buffer, or an array
-		this.t;//type
 		this.i;//index
-		this.c;//context
-		this.process(strwfr, type, indx)
+		this.strowferize(strwfr, type, indx)
     }
 
-	process(strwfr, type, indx){
-		
-		if(this.isString(strwfr) && this.isEncoding(type) && strwfr.length){
-			this.strwfr=strwfr
-			this.t=type
-			this.c='string'
-			if(indx){this.i=indx}
-
-		} else if (((this.isArray(strwfr) && strwfr.length) && (this.isEncoding(type)) || (this.isArray(type) && type.length && (type.length==strwfr.length)))){
-
-			this.strwfr=this.wrap(strwfr, 'row', type)
-			this.t=type
-			this.c='row'
-			if(indx){this.i=indx}
-
-		}else if (this.isBuffer(strwfr) && this.isEncoding(type)&&strwfr.length){
-			this.strwfr=this.wrap(strwfr, 'buffer', type)
-			this.t=type
-			this.c='buffer'
-			if(indx){this.i=indx}
-
-		}else{
-			throw Error(
-				`strwfr must be a string, array, or buffer 
-				OR you need to provide a type (which could 
-				be an array of types for rows, an encoding 
-				for buffers, or an encoding for strings)`
-			)
-		}
+	strowferize(strwfr, type, indx){
+		//whatever it is, it need not be iterable!
+		//Because if it is a string, it should be contained in a single cell for optimization
+		//unless you have a different encoding for each charachter
+		//
 	}
 
-	//if its a string, its not decoded, just returned
-	//if its a buffer, its not decoded, just returned
-	//if its a row, just return an array
-	raw(){
-
-		if(this.c=='string'){
-
-			return [this.strwfr, this.t, this.i]
-
-		}else if(this.c=='buffer' || this.c =='row'){
-
-			var unwrapped = this.unwrap(this.strwfr, this.t, this.c)
-			return [unwrapped[0], unwrapped[1], this.i]
-		
-		}else{
-			throw Error("Strowfer is corrupted, has no context variable defined")
-		}
+	toString(delim){
+		//the problem with a string is that its usually encoded monolithicly
+		//buffers
 	}
-	
-	//takes a stwrfer, context, and type and either
-	//encodes if its a buffer or
-	//creates an array of cell objects if it was an array
-	//creates an encoded string from a string
-	wrap(strwfr, context, type){
-
-		if(context=='row' && this.isArray(strwfr) && this.isEncoding(type)){
-			var arr=[]
-			if(!type){
-				for(var i=0; i<strwfr.length; i++){
-					arr.push(new Cell(strwfr[i], 'utf-8'))
-				}
-	
-			}else{
-	
-				if(this.isArray(type)){
-					
-					if((type.length==strwfr.length)){
-						for(var i=0; i<strwfr.length; i++){
-							arr.push(new Cell(strwfr[i], type[i]))
-						}
-	
-					}else{
-						throw Error("If type is an array, it must be of length strwfr.length")
-					}
-	
-				}else{
-	
-					for(var i=0; i<strwfr.length; i++){
-						arr.push(new Cell(strwfr[i], type))
-					}
-				}
-			}
-
-			return arr
-
-		}else if(context=='buffer'&& this.isBuffer(strwfr) && this.isEncoding(type)){
-
-			return strwfr.toString(type)
-		}else{
-			throw Error("wrapping of", strwfr, context, "failed")
-		}
+	toRow(delim){
 
 	}
-
-	//takes a strwfer, context and either
-	//creates a buffer if context is a buffer
-	//creates a raw array if its an array of cell objects
-	unwrap(strwfr, type, context){
-		if(context=='row' && this.isRow(strwfr)){
-			var vals = []
-			var types = []
-			strwfr.forEach((cell)=>{
-				vals.push(cell.v)
-				types.push(cell.t)
-			})
-			return [vals, types]
-
-		}else if(context=='buffer' && this.isString(strwfr)){
-			return [Buffer.from(strwfr, type), type]
-		}else{
-			throw Error("unwrapping of", strwfr, context, "failed")
-		}
+	toBuffer(){
+		//Buffers are filled with an array of cell objects, because 
+		//these objects are slightly more rich than a string. They can be
+		//encoded uniquely and bufferized (without losing encondoings) or 
+		//stringified which is a one way function that can represent semantics
+		//but not syntax
 	}
+	
 
 	isBuffer(buff){ return Buffer.isBuffer(buff); }
 	
