71bdbc93992b8c2a5d7cb94fa45c38838b59a4b1
Zeus <zeusofolympus1000@gmail.com>
2022-07-22 07:05:03 -0400

    leaving some comments

--- Source/Strowffer/Strowffer.js
+++ Source/Strowffer/Strowffer.js
@@ -3,133 +3,136 @@ import { Cell } from "../Cell.js"
 //we use a Strowffer, even though we do not know how to exploit those mathematical relationships
 //yet. The idea is that if we use them enough, we will find how to exploit that more. 
 export class Strowfer{
-	//if there are no separators defined for a string (if strwfr is a string)
+	//if there are no separators defined for a string (if strofr is a string)
 	//the string is held in a single cell
-	//if the strwfr is an array or buffer, it is (decoded if a buffer and) separated into cells 
-    constructor(strwfr, type='utf-8', indx=undefined){
-		this.strwfr=[];//strwfr = string, buffer, or an array
+	//if the strofr is an array or buffer, it is (decoded if a buffer and) separated into cells 
+    constructor(strofr, type='utf-8', indx=undefined){
+		this.strofr=[];//strofr = string, buffer, or an array
 		this.i=indx;//index
-		this.strowferize(strwfr, type)
+		this.strowferize(strofr, type)
     }
 
-	strowferize(strwfr, type){
-		if(this.isString(strwfr) && this.isEncodedArray(type) && (type.length==strwfr.length)){
-			this._stringEncodedArrayOrStringArrayEncodedArray(strwfr, type) 		
-		}else if(this.isStringArray(strwfr) && this.isEncodedArray(type)){
-			this._stringEncodedArrayOrStringArrayEncodedArray(strwfr, type) 		
-		}else if(this.isStringArray(strwfr) && this.isEncoded(type)){
-			this._stringArrayEncoded(strwfr, type) 									
-		}else if(this.isStringArray(strwfr) && !type){
-			this._stringEncodedArrayOrStringArrayEncodedArray(strwfr, 'utf-32')
-		}else if(this.isString(strwfr) && this.isEncoded(type)){
-			this._stringEncoded(strwfr, type)			
-		}else if(this.isString(strwfr) && !type){		
-			this._stringEncoded(strwfr, 'utf-32')		
-		}else if(this.isString(strwfr) && this.isSeparated(type)){
-			this._stringSeparated(strwfr, type)
+	strowferize(strofr, type){
+		if(this.isString(strofr) && this.isEncodedArray(type) && (type.length==strofr.length)){
+			this._stringEncodedArrayOrStringArrayEncodedArray(strofr, type) 		
+		}else if(this.isStringArray(strofr) && this.isEncodedArray(type)){
+			this._stringEncodedArrayOrStringArrayEncodedArray(strofr, type) 		
+		}else if(this.isStringArray(strofr) && this.isEncoded(type)){
+			this._stringArrayEncoded(strofr, type) 									
+		}else if(this.isStringArray(strofr) && !type){
+			this._stringArrayEncoded(strofr, 'utf-32')
+		}else if(this.isString(strofr) && this.isEncoded(type)){
+			this._stringEncoded(strofr, type)			
+		}else if(this.isString(strofr) && !type){		
+			this._stringEncoded(strofr, 'utf-32')		
+		}else if(this.isString(strofr) && this.isSeparated(type)){
+			this._stringSeparated(strofr, type)
 		}
-		else if(this.isBuffer(strwfr) && this.isEncodedArray(type) ){
-			this._bufferEncodedArray(strwfr, type)
-		}else if(this.isBuffer(strwfr) && this.isEncoded(type)){
-			this._bufferEncoded(strwfr, type)
-		}else if(this.isBuffer(strwfr) && this.isSeparated(type)){
-			this._bufferSeparated(strwfr, type)
-		}else if(this.isBuffer(strwfr) && !type){
-			this._buffer(strwfr)
-		}else if(this.isBufferArray(strwfr) && this.isEncodedArray(type)){
-			this._bufferArrayEncodedArray(strwfr, type)
-		}else if(this.isBufferArray(strwfr) && this.isEncoded(type)){
-			this._bufferArrayEncoded(strwfr, type)
-		}else if(this.isBufferArray(strwfr) && !type){
-			this._bufferArray(strwfr)
-		}else if(this.isArray(strwfr) && this.isEncodedArray(type)){
-			this._arrayEncodedArray(strwfr, type)
-		}else if(this.isArray(strwfr) && this.isEncoded(type)){
-			this._arrayEncoded(strwfr, type)
-		}else if(this.isArray(strwfr) && !type()){
-			this._array(strwfr)
+		else if(this.isBuffer(strofr) && this.isEncodedArray(type) ){
+			this._bufferEncodedArray(strofr, type)
+		}else if(this.isBuffer(strofr) && this.isEncoded(type)){
+			this._bufferEncoded(strofr, type)
+		}else if(this.isBuffer(strofr) && this.isSeparated(type)){
+			this._bufferSeparated(strofr, type)
+		}else if(this.isBuffer(strofr) && !type){
+			this._buffer(strofr, 'utf-32')
+		}else if(this.isBufferArray(strofr) && this.isEncodedArray(type)){
+			this._bufferArrayEncodedArray(strofr, type)
+		}else if(this.isBufferArray(strofr) && this.isEncoded(type)){
+			this._bufferArrayEncoded(strofr, type)
+		}else if(this.isBufferArray(strofr) && !type){
+			this._bufferArray(strofr, 'utf-32')
+		}else if(this.isArray(strofr) && this.isEncodedArray(type)){
+			this._arrayEncodedArray(strofr, type)
+		}else if(this.isArray(strofr) && this.isEncoded(type)){
+			this._arrayEncoded(strofr, type)
+		}else if(this.isArray(strofr) && !type()){
+			this._array(strofr)
 		}else{
 			throw Error("Strowfers do not accept giberish, unless its actual giberish")
 		}
 	}
-
-	_stringEncodedArrayOrStringArrayEncodedArray(strwfr, type){
-		for(var i = 0; i<strwfr.length; i++){
-			this.strwfr.push(new Cell(strwfr[i], type[i]))
+	//it doesn't matter if strofr is an array or string, they act the same here
+	_stringEncodedArrayOrStringArrayEncodedArray(strofr, type){
+		for(var i = 0; i<strofr.length; i++){
+			this.strofr.push(new Cell(strofr[i], type[i]))
 		}
-		
 	}
-
-	_stringArrayEncoded(strwfr, type){
-		strwfr.forEach((str)=>{
-			this.strwfr.push(new Cell(str, type))
-		})
+	//string array, single encoding type
+	_stringArrayEncoded(strofr, type){
+		for(var i = 0; i<strofr.length; i++){
+			this.strofr.push(new Cell(strofr[i], type))
+		}
 	}
-
-	_stringEncoded(strwfr, type){
-		this.strwfr.push(new Cell(strwfr, type))
+	//single string single encoding
+	_stringEncoded(strofr, type){
+		this.strofr.push(new Cell(strofr, type))
 	}
-
-	_stringSeparated(strwfr, type){
-		var splits = strwfr.split(type)
+	//single string utf-32 (inferred), separated by sep
+	_stringSeparated(strofr, sep){
+		var splits = strofr.split(sep)
 		splits.forEach((splt)=>{
-			this.strwfr.push(new Cell(splt, 'utf-32'))
+			this.strofr.push(new Cell(splt, 'utf-32'))
 		})
 	}
-
-	_bufferEncodedArray(strwfr, type){
-		strwfr = Buffer.from(strwfr, 'utf-32')
-		this._stringEncodedArrayOrStringArrayEncodedArray(strwfr, type)
+	//single string buffer decoded utf-32 (inferred), with code array for the string positions
+	_bufferEncodedArray(strofr, type){
+		strofr = Buffer.from(strofr, 'utf-32')
+		this._stringEncodedArrayOrStringArrayEncodedArray(strofr, type)
 	}
 
-	_bufferEncoded(strwfr, type){
-		strwfr = Buffer.from(strwfr, type)
-		this._stringEncoded(strwfr, type)
+	//single buffer, single encoding
+	_bufferEncoded(strofr, type){
+		strofr = Buffer.from(strofr, type)
+		this._stringEncoded(strofr, type)
 	}
 
-	_bufferSeparated(strwfr, type){
-		strwfr = Buffer.from(strwfr, 'utf-32')
-		this._stringSeparated(strwfr, type)
+	//single buffer decoded utf-32 (inferred), separated by sep
+	_bufferSeparated(strofr, sep){
+		strofr = Buffer.from(strofr, 'utf-32')
+		this._stringSeparated(strofr, sep)
+	}
+	//single buffer, utf-32 decoding inferred
+	_buffer(strofr){
+		strofr = Buffer.from(strofr, 'utf-32')
+		this._stringEncoded(strofr, 'utf-32')
 	}
 
-	_buffer(strwfr){
-		strwfr = Buffer.from(strwfr, 'utf-32')
-		this._stringEncoded(strwfr, 'utf-32')
-	}
-
-	_bufferArrayEncodedArray(strwfr, type){
+	//buffer strings decoded using utf32, then types in array stored with respective strings
+	_bufferArrayEncodedArray(strofr, type){
 		var strs=[]
-		strwfr.forEach((str)=>{
+		strofr.forEach((str)=>{
 			strs.push(Buffer.from(str, 'utf-32'))
 		})
 		this._stringEncodedArrayOrStringArrayEncodedArray(strs, type)
 	}
 
-	_bufferArrayEncoded(strwfr, type){
-		strwfr = Buffer.from(strwfr, type)
-		this._stringArrayEncoded(strwfr, type)
+	//
+	_bufferArrayEncoded(strofr, type){
+		strofr = Buffer.from(strofr, type)
+		this._stringArrayEncoded(strofr, type)
 	}
 
-	_bufferArray(strwfr){
-		strwfr = Buffer.from(strwfr, 'utf-32')
-		this._stringArrayEncoded(strwfr, 'utf-32')
+	_bufferArray(strofr){
+		strofr = Buffer.from(strofr, 'utf-32')
+		this._stringArrayEncoded(strofr, 'utf-32')
 	}
 
-	_arrayEncodedArray(strwfr, type){
-		for(var i = 0; i<strwfr.length; i++){
-			this.strwfr.push(new Cell(strwfr[i], type[i]))
+	_arrayEncodedArray(strofr, type){
+		for(var i = 0; i<strofr.length; i++){
+			this.strofr.push(new Cell(strofr[i], type[i]))
 		}
 	}
 
-	_arrayEncoded(strwfr, type){
-		strwfr.forEach((obj)=>{
-			this.strwfr.push(new Cell(obj, type))
+	_arrayEncoded(strofr, type){
+		strofr.forEach((obj)=>{
+			this.strofr.push(new Cell(obj, type))
 		})
 	}
 	//sometimes an array of objects is typeless by design and should not be inferred
-	_array(strwfr){
-		strwfr.forEach((obj)=>{
-			this.strwfr.push(new Cell(obj))
+	_array(strofr){
+		strofr.forEach((obj)=>{
+			this.strofr.push(new Cell(obj))
 		})
 	}
 
@@ -177,8 +180,8 @@ export class Strowfer{
 			return true
 		}
 	}
-	isRow(strwfr){
-		strwfr.forEach((cell)=>{
+	isRow(strofr){
+		strofr.forEach((cell)=>{
 			if(!this.isCell(cell)){return false}
 		})
 		return true
